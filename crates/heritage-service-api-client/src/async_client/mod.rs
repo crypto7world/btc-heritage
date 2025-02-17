d08800af1f1508c7794cf68d6290e6d2830e92f0
John GALT <john@crypto7.world>
2025-02-17 17:40:08 +0100

    Fix - heritage-service-api-client - Re-Exposing the DeviceAuthorizationResponse type

--- crates/heritage-service-api-client/src/async_client/mod.rs
+++ crates/heritage-service-api-client/src/async_client/mod.rs
@@ -2,5 +2,5 @@ pub(crate) mod auth;
 pub(crate) mod client;
 
 pub use crate::errors::Error;
-pub use auth::{TokenCache, Tokens};
+pub use auth::{DeviceAuthorizationResponse, TokenCache, Tokens};
 pub use client::HeritageServiceClient;
